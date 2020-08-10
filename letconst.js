ee1a80ff230676c8c983ccb3a673ab108cffc29a
sagorwebdeveloper <67562467+sagorwebdeveloper@users.noreply.github.com>
2020-08-10 20:23:11 +0600

    let const done

--- letconst.js
+++ letconst.js
@@ -1,6 +1,6 @@
 //Let, const, array declared with const, object declared with const:
 const numbers=[12,88,55];
-numbers[1]=52;
+numbers[1] = 52;
 numbers.push(12);
 //numbers=[22,44];-not acceptable.
 //numbers=["solviya",sagor];-not acceptable.
